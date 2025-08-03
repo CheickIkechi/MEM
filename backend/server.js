59f68a6cac19207a2eeda3b5169ac523a82b7bf1
CheickIkechi <cheickawamini14@gmail.com>
2025-08-03 14:56:18 +0200

    v4

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`➡️ Nouvelle requête : ${req.method} ${req.originalUrl}`);
+  next();
+});
+
 // Routes publiques
 app.use('/api/auth', authRoutes);
 
